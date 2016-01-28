e30eb98b38d66517d51b5361fa78d9523296d907
Joseph DuBay <dubie1977@Josephs-iMac.local>
2016-01-27 21:23:50 -0800

    strated logic

--- RetroCalc/ViewController.swift
+++ RetroCalc/ViewController.swift
@@ -9,6 +9,14 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    //outlets
+    @IBOutlet weak var valueLbl: UILabel!
+    
+    
+    //local vars
+    enum Operator { case add; case divide}
+    var currentOperator: Operator = Operator.add
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +27,27 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    
+    //actions
+    @IBAction func numberPressed(button: UIButton){
+    
+    }
+    
+    @IBAction func operatorPressed(button: UIButton){
+       switch currentOperator {
+            case .add:
+                print("adding")
+                break
+            default:
+                print("default")
+                break
+        }
+    }
+    
+    @IBAction func clearPressed(button: UIButton){
+    
+    }
 
 }
 
